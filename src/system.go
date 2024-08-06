fa2ce97605547f11b26736fe36e4d3cba53c281c
Dan Worley <danworley@gmail.com>
2024-08-06 12:53:16 -0400

    Cleanup bundledir lookup

--- src/system.go
+++ src/system.go
@@ -91,6 +91,7 @@ var sys = System{
 	stereoEffects:        true,
 	panningRange:         30,
 	windowCentered:       true,
+	bundleDir:	      "",
 }
 
 type TeamMode int32
@@ -354,6 +355,7 @@ type System struct {
 	frameCounter      int32
 	preFightTime      int32
 	motifDir          string
+	bundleDir	  string
 	captureNum        int
 	roundType         [2]RoundType
 	timerStart        int32
